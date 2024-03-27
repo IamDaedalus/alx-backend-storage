5df3ba080c67d2ab00ddd32b2609009ff4b7d258
Daedalus <manny.is.daedalus@pm.me>
2024-03-27 15:55:46 +0000

    task 0 redo

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,22 +10,11 @@ from redis.commands.core import ResponseT
 class Cache:
     def __init__(self):
         """ this is the init method """
-        self._redis: redis.Redis = redis.Redis()
+        self._redis = redis.Redis()
         self._redis.flushdb()
-
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ this is the store method """
-        random_key: str = str(uuid.uuid4())
+        random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-
-
-    def get(self, key: str, fn: Optional[Callable]) -> Union[
-            str, bytes, int, float, ResponseT]:
-        """ this is the get method """
-        value = self._redis.get(key)
-        if fn is not None and value is not None:
-            return fn(value)
-
-        return value
